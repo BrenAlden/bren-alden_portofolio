0ecf98d205441c45bd360df86966c3a2a19b5e48
Bren Alden <164768427+BrenAlden@users.noreply.github.com>
2025-06-17 16:55:09 +0700

    Update main.jsx

--- portofolio_bren-alden/src/main.jsx
+++ portofolio_bren-alden/src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './styles/App.css'; 
 import App from './App'; 
 
-import { BrowserRouter } from 'react-router-dom';
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
