d47fae1ffcf2015cf48409041ab79c9fca7bc54b
Bren Alden <164768427+BrenAlden@users.noreply.github.com>
2025-06-17 16:50:20 +0700

    Update main.jsx

--- portofolio_bren-alden/src/main.jsx
+++ portofolio_bren-alden/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bren-alden_portofolio/"> {/* <--- PERIKSA INI */}
+    <BrowserRouter> {/* <--- PERIKSA INI */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
