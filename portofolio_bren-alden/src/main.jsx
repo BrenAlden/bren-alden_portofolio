5c58a9faabf14444e8e90ec41eeb0266d7e0bd72
Bren Alden <164768427+BrenAlden@users.noreply.github.com>
2025-06-17 16:53:01 +0700

    Update main.jsx

--- portofolio_bren-alden/src/main.jsx
+++ portofolio_bren-alden/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './styles/App.css'; 
 import App from './App'; 
 
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <--- PERIKSA INI */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
